8c9b3de7cd35ff455eddb1043cf8ac65ae5c9ae1
Bruno Bispo <brunobispoo25@gmail.com>
2022-11-10 21:12:44 -0300

    Removida a pasta dist.

--- dist/src/router/authenticationRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authentication_1 = require("../controllers/authentication");
-const authenticationMiddleware_1 = require("../middlewares/authenticationMiddleware");
-const router = (0, express_1.Router)();
-router.post('/', authenticationMiddleware_1.validateBodyAuthentication, authentication_1.loginUser);
-exports.default = router;
